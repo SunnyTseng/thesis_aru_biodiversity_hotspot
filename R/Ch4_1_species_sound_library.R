eb28443a0d0ab7d712c8b60f77c155851a1100b4
Sunny Tseng <sunnyyctseng@gmail.com>
2023-07-04 11:38:57 -0700

    updated analysis code

--- R/Ch4_1_species_sound_library.R
+++ R/Ch4_1_species_sound_library.R
@@ -11,9 +11,8 @@ library(tidyverse)
 library(here)
 
 
-
 ###
-### Data preparation
+### Get the validation dataset ready (set threshold as 0.85)
 ###
 
 # Detection list from 2020, 2021, and 2022 passerine
@@ -30,118 +29,24 @@ for (file in file_names){
 data_all_0.85 <- data_all %>%
   filter(confidence >= 0.85) 
 
-# Make a list of potential recordings based on individual species
-species_recording <- data_all_0.85 %>%
-  unite("recording_u", site, recording, start_s, end_s, sep = "_") %>%
-  arrange(desc(confidence)) %>%
-  group_nest(common_name) %>%
-  mutate(recording_1 = map_chr(.x = data, .f =~ .x %>% pull(recording_u) %>% .[1]),
-         recording_2 = map_chr(.x = data, .f =~ .x %>% pull(recording_u) %>% .[2]),
-         recording_3 = map_chr(.x = data, .f =~ .x %>% pull(recording_u) %>% .[3]),
-         recording_4 = map_chr(.x = data, .f =~ .x %>% pull(recording_u) %>% .[4]),
-         recording_5 = map_chr(.x = data, .f =~ .x %>% pull(recording_u) %>% .[5])) %>%
-  select(-data)
-# write_csv(species_recording, here("Ch0_JPRF_library", "docs", "species_list_above_85.csv"))
-
-
-
 
 ###
-### Recording validation
-###
-# listen to the target recordings
-species <- 126 # range from 1 to 126
-for (info in 2:6) {
-  file <- species_recording[species, info] %>%
-    str_split(pattern = "_") %>%
-    unlist()
-  
-  dir <- paste0("E:/Audio/", str_sub(file[3], 1,4), "_passerine")
-  site <- paste0(file[1], "_", file[2])
-  recording <- paste0(file[3], "_", file[4])
-  start_s <- file[5] %>% as.numeric()
-  end_s <- file[6] %>% as.numeric()
-  song <- readWave(paste0(dir, "/", site, "/", recording, ".wav"), 
-                   from = (start_s - 2), 
-                   to = (end_s + 2), 
-                   units = "seconds")
+### Data preparation for ARU bird list
+### 
 
-  print(paste0("This is recording ", recording, " from ", start_s, " to ", end_s))
-  play(song, ... = "/play /close")
-}
+# Filter out the non-validated species and species (108) suggested by Ken that shouldn't be here (103)
+data_validated <- read_csv(here("data", "JPRF_species_list",  "species_list_above_85_validation.csv")) %>%
+  drop_na(best) %>%
+  filter(!common_name %in% c("Pygmy Nuthatch", "Red-naped Sapsucker", "Williamson's Sapsucker", "Bay-breasted Warbler", "Bushtit", "Northwestern Crow", "Slate-colored Fox Sparrow", "Audubon's Warbler"))
 
-# plot out spectrum whenever needed
-spec <- spectro(song, flim = c(0, 5), tlim = c(2, 3))
+clements_species <- read_csv(here("data", "JPRF_species_list", "Clements-Checklist-v2022-October-2022.csv"))
 
-
-
-###
-### Creating folders and move soundfiles around
-###
-
-# import the validation datasheet 
-data_validated <- read_csv(here("Ch0_JPRF_library", "docs", "species_list_above_85_validation.csv"))
-
-data_validated_cleaned <- data_validated %>%
-  drop_na(best) 
-
-# ready for for loop. Here we are doing a species for first iteration
-for (j in 1:nrow(data_validated_cleaned)){
-  
-  species <- data_validated_cleaned[j,]
-  
-  # create file folder
-  dir.create(here("Ch0_JPRF_library", "data", species$common_name))
-  
-  # For single species, iteration for 5 recordings (if true) and rename the best one
-  for (i in 7:11) {
-    if (species[i] == "Y") {
-      
-      file <- species[i-5] %>%
-        str_split(pattern = "_") %>%
-        unlist()
-      
-      year <- str_sub(file[3], 1,4)
-      site <- paste0(file[1], "_", file[2])
-      recording <- paste0(file[3], "_", file[4])
-      dir <- paste0("E:/Audio/", year, "_passerine")
-      
-      from_file <- paste0(dir, "/", site, "/", recording, ".WAV")
-      to_folder <- here("Ch0_JPRF_library", "data", species$common_name)
-      
-      # file copy and rename
-      file.copy(from_file, to_folder)
-      
-      file.rename(paste0(here("Ch0_JPRF_library", "data", species$common_name), "/", 
-                         recording, ".WAV"),
-                  paste0(here("Ch0_JPRF_library", "data", species$common_name), "/", 
-                         year, "_", site, "_", recording, ".WAV"))
-    }
-  }
-  
-  best_index <- as.numeric(species[12]) + 6
-  
-  file <- species[best_index - 5] %>%
-    str_split(pattern = "_") %>%
-    unlist()
-  
-  year <- str_sub(file[3], 1,4)
-  site <- paste0(file[1], "_", file[2])
-  recording <- paste0(file[3], "_", file[4])
-  dir <- paste0("E:/Audio/", year, "_passerine")
-  
-  file.rename(paste0(here("Ch0_JPRF_library", "data", species$common_name), "/", 
-                     year, "_", site, "_", recording, ".WAV"),
-              paste0(here("Ch0_JPRF_library", "data", species$common_name), "/", 
-                     year, "_", site, "_", recording, "_A", ".WAV"))
-}
-
-
-###
-### Summarizing data by species (how many detections were received for each of the confidence range, 
-### how many ARUs out of 66 were getting that specific species?)
-###
-data_species <- data_all %>%
+data_species <- data_all_0.85 %>%
+  mutate(common_name = case_when(
+    common_name == "Audubon's Warbler" ~ "Yellow-rumped Warbler",
+    common_name == "Northwestern Crow" ~ "American Crow",
+    common_name == "Slate-colored Fox Sparrow" ~ "Fox Sparrow",
+    TRUE ~ common_name)) %>%
   mutate(confidence_cat = cut(confidence, breaks = seq(from = 0.1, to = 1.0, by = 0.15))) %>%
   group_nest(common_name) %>%
   mutate(c_10_25 = map_dbl(.x = data, .f =~ .x %>% filter(confidence_cat == "(0.1,0.25]") %>% nrow()),
@@ -174,8 +79,193 @@ data_species <- data_all %>%
          n_ARU_2022 = map_dbl(.x = data, .f =~ .x %>% filter(year == 2022) %>% pull(site) %>% n_distinct())) %>%
   select(-data)
 
-data_validated <- read_csv(here("docs", "species_list_above_85_validation.csv"))
 
-data_species_all <- left_join(data_validated, data_species)
-write_csv(data_species_all, here("docs", "species_list_above_85_validation_info.csv"))
+data_validated_1 <- left_join(data_validated, data_species) %>%
+  mutate(common_name = case_when(
+    common_name == "American Yellow Warbler" ~ "Yellow Warbler",
+    common_name == "Audubon's Warbler" ~ "Yellow-rumped Warbler (Audubon's)",
+    common_name == "Northwestern Crow" ~ "American Crow",
+    common_name == "Slate-colored Fox Sparrow" ~ "Fox Sparrow (Slate-colored)",
+    TRUE ~ common_name)) %>%
+  left_join(clements_species, by = c("common_name" = "English name")) %>%
+  select(common_name, "scientific name", "order", "family", 14:41)
+
+
+# write_csv(data_validated_1, here("data", "JPRF_species_list", "species_list_above_85_validation_info.csv"))
+
+
+
+###
+### Recording validation for the 0.85 confidence (126 species for validation)
+###
+
+# Make a list of potential recordings based on individual species
+species_recording <- data_all_0.85 %>%
+  unite("recording_u", site, recording, start_s, end_s, sep = "_") %>%
+  arrange(desc(confidence)) %>%
+  group_nest(common_name) %>%
+  mutate(recording_1 = map_chr(.x = data, .f =~ .x %>% pull(recording_u) %>% .[1]),
+         recording_2 = map_chr(.x = data, .f =~ .x %>% pull(recording_u) %>% .[2]),
+         recording_3 = map_chr(.x = data, .f =~ .x %>% pull(recording_u) %>% .[3]),
+         recording_4 = map_chr(.x = data, .f =~ .x %>% pull(recording_u) %>% .[4]),
+         recording_5 = map_chr(.x = data, .f =~ .x %>% pull(recording_u) %>% .[5])) %>%
+  select(-data)
+# write_csv(species_recording, here("Ch0_JPRF_library", "docs", "species_list_above_85.csv"))
+
+# listen to the target recordings
+species <- 126 # range from 1 to 126 for 126 species
+
+for (info in 2:6) { # don't change this, it is for the column number
+  file <- species_recording[species, info] %>%
+    str_split(pattern = "_") %>%
+    unlist()
+  
+  dir <- paste0("E:/Audio/", str_sub(file[3], 1,4), "_passerine")
+  site <- paste0(file[1], "_", file[2])
+  recording <- paste0(file[3], "_", file[4])
+  start_s <- file[5] %>% as.numeric()
+  end_s <- file[6] %>% as.numeric()
+  song <- readWave(paste0(dir, "/", site, "/", recording, ".wav"), 
+                   from = (start_s - 2), 
+                   to = (end_s + 2), 
+                   units = "seconds")
+
+  print(paste0("This is recording ", recording, " from ", start_s, " to ", end_s))
+  play(song, ... = "/play /close")
+}
+
+# plot out spectrum whenever needed
+spec <- spectro(song, flim = c(0, 5), tlim = c(2, 3))
+
+
+
+###
+### Creating folders and move soundfiles around for the validated sounds (108 species)
+###
+
+# ready for for loop. Here we are doing a species for first iteration
+for (j in 1:nrow(data_validated_cleaned)){
+  
+  species <- data_validated_cleaned[j,]
+  
+  # create file folder
+  dir.create(here("data", "JPRF_summer_bird_list_and_sound", species$common_name))
+  
+  # For single species, iteration for 5 recordings (if true) and rename the best one
+  for (i in 7:11) {
+    if (species[i] == "Y") {
+      
+      file <- species[i-5] %>%
+        str_split(pattern = "_") %>%
+        unlist()
+      
+      year <- str_sub(file[3], 1,4)
+      site <- paste0(file[1], "_", file[2])
+      recording <- paste0(file[3], "_", file[4])
+      dir <- paste0("E:/Audio/", year, "_passerine")
+      
+      from_file <- paste0(dir, "/", site, "/", recording, ".WAV")
+      to_folder <- here("data", "JPRF_summer_bird_list_and_sound", species$common_name)
+      
+      # file copy and rename
+      file.copy(from_file, to_folder)
+      
+      file.rename(paste0(here("data", "JPRF_summer_bird_list_and_sound", species$common_name), "/", recording, ".WAV"),
+                  paste0(here("data", "JPRF_summer_bird_list_and_sound", species$common_name), "/", year, "_", site, "_", recording, ".WAV"))
+    }
+  }
+  
+  best_index <- as.numeric(species[12]) + 6
+  
+  file <- species[best_index - 5] %>%
+    str_split(pattern = "_") %>%
+    unlist()
+  
+  year <- str_sub(file[3], 1,4)
+  site <- paste0(file[1], "_", file[2])
+  recording <- paste0(file[3], "_", file[4])
+  dir <- paste0("E:/Audio/", year, "_passerine")
+  
+  file.rename(paste0(here("data", "JPRF_summer_bird_list_and_sound", species$common_name), "/", year, "_", site, "_", recording, ".WAV"),
+              paste0(here("data", "JPRF_summer_bird_list_and_sound", species$common_name), "/", year, "_", site, "_", recording, "_A", ".WAV"))
+}
+
+
+###
+### Data preparation for eBird bird list
+###
+
+data_observed <- read_csv(here("data", "JPRF_species_list", "ebird_JPRF.csv"))
+clements_species <- read_csv(here("data", "JPRF_species_list", "Clements-Checklist-v2022-October-2022.csv"))
+
+data_observed_1 <- data_observed %>%
+  mutate(species = case_when(
+    species == "Eurasian Collared Dove" ~ "Eurasian Collared-Dove",
+    species == "Eurasian/Hudsonian Whimbrel" ~ "Whimbrel",
+    species == "Western Wood Pewee" ~ "Western Wood-Pewee",
+    species == "American Dusky Flycatcher" ~ "Dusky Flycatcher",
+    species == "Northern Raven" ~ "Common Raven",
+    species == "Two-barred Crossbill" ~ "White-winged Crossbill",
+    species == "Myrtle/Audubon's Warbler" ~ "Yellow-rumped Warbler",
+    TRUE ~ species)) %>%
+  left_join(clements_species, by = c("species" = "English name")) %>%
+  select(species, "scientific name", order, family, 2:50) %>%
+  drop_na("scientific name")
+
+# write_csv(data_observed_1, here("data", "JPRF_species_list", "eBird_JPRF_info.csv"))
+
+
+###
+### Species comparison between eBird and ARU (2023 Jun. 21) 
+###
+data_ARU <- read_csv(here("data", "JPRF_species_list", "species_list_above_85_validation_info.csv"))
+data_eBird <- read_csv(here("data", "JPRF_species_list", "eBird_JPRF_info.csv"))
+
+aru_only_species <- setdiff(data_ARU$common_name, data_eBird$species) # 31
+eBird_only_species <- setdiff(data_eBird$species, data_ARU$common_name) # 24, mostly waterfowl
+both_species <- intersect(data_eBird$species, data_ARU$common_name) # 69, these would be common species
+all_species <- union(data_eBird$species, data_ARU$common_name) # 124 species in JPRF wooow!
+
+
+###
+### ARU species detected in each of the sites
+###
+
+species_ARU <- read_csv(here("data", "JPRF_species_list", "species_list_above_85_validation_info.csv")) %>%
+  pull(common_name)
+
+file_names <- c("2020_passerine_BirdNET.csv", 
+                "2021_passerine_BirdNET.csv",
+                "2022_passerine_BirdNET.csv")
+data_all <- tibble()
+for (file in file_names){
+  data <- read_csv(here("data", "JPRF_2020_2021_2022_sound_processed", file))
+  data_all <- bind_rows(data_all, data)
+}
+
+# Clean out the detection with confidence higher than 0.85, got 126 unique species
+data_ARU_sites <- data_all %>%
+  filter(confidence >= 0.85) %>% # get right detections
+  mutate(common_name = case_when( # get the common name correct
+    common_name == "American Yellow Warbler" ~ "Yellow Warbler",
+    common_name == "Audubon's Warbler" ~ "Yellow-rumped Warbler",
+    common_name == "Northwestern Crow" ~ "American Crow",
+    common_name == "Slate-colored Fox Sparrow" ~ "Fox Sparrow",
+    TRUE ~ common_name))  %>%
+  filter(common_name %in% species_ARU) %>% # only keep the species of interest
+  group_nest(site) %>%
+  mutate(richness = map_dbl(.x = data, .f =~ .x %>% pull(common_name) %>% n_distinct()),
+         ARU_day = map_dbl(.x = data, .f =~ .x %>% distinct(year, month, day) %>% nrow()))
+
+
+  
+
+
+
+
+
+
+
+
+
 
